5740274a8449a558698c92e14f34c9734fb3c16d
Kayla Inoa <kinoa718@gmail.com>
2026-05-11 23:43:45 -0400

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        notebook = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n 
+            if diff in notebook:
+                return [notebook[diff], i]
+            notebook[n] = i 
+        return
